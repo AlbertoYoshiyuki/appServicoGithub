b0e6fb0a33dd690a5e08e91b88aa5f37a596ab6b
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-24 22:31:48 -0300

    Initial commit

---
+++ app/src/main/java/com/yoshiyuki/alberto/appservico/GitHubService.kt
@@ -0,0 +1,10 @@
+package com.yoshiyuki.alberto.appservico
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface GitHubService {
+    @GET("/users/{username}")
+fun buscarUsuario(@Path("username") username: String) : Call<Usuario>
+}
